da947d912007b6e23316480bdb5fe442ad4723e6
Jonathan Ware <jonathan.ware02@gmail.com>
2023-07-17 12:20:33 -0500

    minor changes to function in py file

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -15,13 +15,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def new_mall_data():
-    """
-    Fetches mall data from the database using SQL query.
-
-    Returns:
-    - df: DataFrame containing mall data
-    """
-   
+     
     conn = get_db_url('mall_customers')
 
     query = '''
@@ -33,20 +27,22 @@ def new_mall_data():
     df = pd.read_sql(query, conn)
     return df
 
+def get_mall_data():
+   
+    if os.path.isfile('mall_df.csv'):
+        df = pd.read_csv('mall_df.csv', index_col = 0)
+        
+
+    else:
+
+        df = new_mall_data()
+        df.to_csv('mall_df.csv')
+        
+    return df
+
 # -----------------------------outliers--------------------------------
 
 def detect_outliers_iqr(df, columns, threshold=1.5):
-    """
-    Detect outliers in the specified numeric column(s) of a DataFrame using the IQR method.
-    
-    Args:
-        df (pandas.DataFrame): The DataFrame to detect outliers.
-        columns (list or str): The column name(s) or list of column names to examine for outliers.
-        threshold (float, optional): The threshold value to determine outliers (default is 1.5).
-        
-    Returns:
-        pandas.DataFrame: A DataFrame containing the outliers for the specified column(s).
-    """
     if isinstance(columns, str):
         columns = [columns]
     
@@ -67,21 +63,34 @@ def detect_outliers_iqr(df, columns, threshold=1.5):
         outliers = pd.concat([outliers, outliers_col], ignore_index=True)
     
     return outliers
+# -----------------------------prep--------------------------------
+def prep_mall_data(df):
+    
+    
+    def handle_missing_values(df, prop_required_column=0.5, prop_required_row=0.5):
+       
+        threshold_col = int(round(prop_required_column * len(df.index)))
+        df = df.dropna(thresh=threshold_col, axis=1)
+    
+        threshold_row = int(round(prop_required_row * len(df.columns)))
+        df = df.dropna(thresh=threshold_row)
+    
+        return df
 
+    df = handle_missing_values(df)
+    
+    encoded_cols = pd.get_dummies(df['gender'], prefix='gender')
+    df = pd.concat([df, encoded_cols], axis=1)
+    
+    train, validate, test = split_mall_data(df)
+    
+    return train, validate, test
 # -----------------------------split--------------------------------
 
-def split_zillow_data(df):
-    """
-    Splits the Mall data into training, validation, and test sets.
 
-    Arguments:
-    - df: DataFrame containing Mall data
 
-    Returns:
-    - train: DataFrame of the training set
-    - validate: DataFrame of the validation set
-    - test: DataFrame of the test set
-    """
+def split_mall_data(df):
+    
   
     train_validate, test = train_test_split(df, test_size=.2, random_state=123)
     train, validate = train_test_split(train_validate, 
@@ -91,47 +100,11 @@ def split_zillow_data(df):
 
     
     return train, validate, test
-
-# -----------------------------missingval--------------------------------
-
-def handle_missing_values(df, prop_required_column=0.5, prop_required_row=0.5):
-        """
-        Drops columns and rows from a DataFrame based on the percentage of missing values.
-
-        Args:
-            df (pandas.DataFrame): The DataFrame to handle missing values.
-            prop_required_column (float, optional): The minimum proportion of non-null values required for columns (default is 0.5).
-            prop_required_row (float, optional): The minimum proportion of non-null values required for rows (default is 0.5).
-
-        Returns:
-            pandas.DataFrame: The DataFrame with dropped rows and columns.
-        """
-
-        threshold_col = int(round(prop_required_column * len(df.index)))
-        df = df.dropna(thresh=threshold_col, axis=1)
-
-
-        threshold_row = int(round(prop_required_row * len(df.columns)))
-        df = df.dropna(thresh=threshold_row)
-
-        return df
     
 # -----------------------------scale--------------------------------
     
 def scale_data(train, validate, test):
-    """
-        Scales numerical columns in the train, validate, and test sets using MinMaxScaler.
-
-        Arguments:
-        - train: DataFrame of the training set
-         - validate: DataFrame of the validation set
-        - test: DataFrame of the test set
-
-        Returns:
-        - train_scaled: Scaled training set DataFrame
-         - validate_scaled: Scaled validation set DataFrame
-        - test_scaled: Scaled test set DataFrame
-         """
+    
     
     numeric_cols = ['spending_score','annual_income','age']
     
@@ -147,3 +120,10 @@ def scale_data(train, validate, test):
     test_scaled[numeric_cols] = scaler.transform(test[numeric_cols])
     
     return train_scaled, validate_scaled, test_scaled
+
+
+def wrangle_mall():
+  
+    df = get_mall_data()
+    train, validate, test = prep_mall_data(df)
+    return train, validate, test
